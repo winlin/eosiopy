bad18f795f5e1ed494433e1b5eab5c3f5613f0bc
eosmoto <eosmoto@163.com>
2018-07-03 17:25:03 +0800

    change travis

--- eosiopy/__init__.py
+++ eosiopy/__init__.py
@@ -4,7 +4,7 @@ from .nodenetwork import NodeNetwork
 from .rawinputparams import RawinputParams
 from .sign import sign
 
-VERSION = (0, 1, 1)
+VERSION = (0, 1, 2)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
