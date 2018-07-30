af777d3db687f3daf6630059ad8a1f5f25e9224c
eosmoto <eosmoto@163.com>
2018-07-30 18:07:51 +0800

    add double test

--- test/test.py
+++ test/test.py
@@ -24,6 +24,19 @@ class EosioPyTestCase(unittest.TestCase):
         net = NodeNetwork.push_transaction(eosiop_arams.trx_json)
         self.assertTrue(isinstance(net, dict))
         self.assertNotEqual(net.get("transaction_id", None), None)
+        raw = RawinputParams("newaccount", {"creator": "eosio", "name": "aeztcnmmhege", "owner": {"threshold": 1,
+                                                                                                  "keys": [{
+                                                                                                      "key": "EOS5XhYEWPJH2P77rh6dwMURsgjFodjXqTadSf1tNZAHVhnBpWyNu",
+                                                                                                      "weight": 1}],
+                                                                                                  "accounts": [],
+                                                                                                  "waits": []},
+                                            "active": {"threshold": 1, "keys": [
+                                                {"key": "EOS7EZgfh13yVxaXuzH12cC2Yru7Wv1JNNNbxnSdZQXNX2hWAyBTm",
+                                                 "weight": 1}], "accounts": [], "waits": []}}, "eosio", "eosio@active")
+        eosiop_arams = EosioParams(raw.params_actions_list, "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3")
+        net = NodeNetwork.push_transaction(eosiop_arams.trx_json)
+        self.assertTrue(isinstance(net, dict))
+        self.assertNotEqual(net.get("transaction_id", None), None)
 
 
 if __name__ == '__main__':
