9ece54ee60a60062aa18a1c2358d34d372af7ef6
eosmoto <eosmoto@163.com>
2018-08-01 10:28:03 +0800

    Revert "add verify config"
    This reverts commit 195b054

--- eosiopy/__init__.py
+++ eosiopy/__init__.py
@@ -4,7 +4,7 @@ from .nodenetwork import NodeNetwork
 from .rawinputparams import RawinputParams
 from .sign import sign
 
-VERSION = (0, 1, 3)
+VERSION = (0, 1, 4)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
